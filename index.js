60b3afa08671b5aa74848120ca38ce7fce9fa053
Dk <119879571+DanielRoy139@users.noreply.github.com>
2023-01-03 14:07:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -115,3 +115,22 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("top");
+    } else {
+      reveals[i].classList.remove("top");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
